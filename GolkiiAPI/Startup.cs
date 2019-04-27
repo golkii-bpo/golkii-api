f964882e2a432bb865ff3e4f962103d67161d85e
Sergio Lopez <serlopsal_199@yahoo.es>
2019-04-27 12:29:17 -0600

    Swagger api/docs

--- GolkiiAPI/Startup.cs
+++ GolkiiAPI/Startup.cs
@@ -62,13 +62,17 @@ namespace GolkiiAPI
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
+            //    (c =>
+            //{
+            //    c.RouteTemplate = "api/swagger/{documentname}/swagger.json";
+            //});
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "GolkiiAPI V1");
-                c.RoutePrefix = string.Empty;
+                c.RoutePrefix = "api/docs";
             });
 
             app.UseMvc();
